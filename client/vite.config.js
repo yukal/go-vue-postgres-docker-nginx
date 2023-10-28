8eeea4e38e3fa3ec0b153cba26b2a0af3122ba9c
Alexander Yukal <yukal.alexander@gmail.com>
2023-10-28 15:28:37 +0300

    refactor: improve vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,4 +13,20 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  server: {
+    // host: 'localhost',
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:50598/api/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/img': {
+        target: 'http://localhost:50598/img/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/img/, ''),
+      },
+    },
+  },
 })
